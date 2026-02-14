375695fc1ab75c4541274c49264839bca752741d
matheus-vsm <scompinha@hotmail.com>
2026-02-13 23:31:00 -0300

    aula 2.6

---
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,15 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+    @RabbitListener(queues = "pagamento.concluido")
+    public void receberMensagem(Message mensagem) {
+        System.out.println("Mensagem recebida: " + mensagem.toString());
+    }
+
+}
